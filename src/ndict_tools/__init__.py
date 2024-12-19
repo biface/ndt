9a08d437694f4008fc40ff5ae4d649cb2c1ef29f
biface <fabrice.moutte.wn@gmail.com>
2024-12-19 21:28:41 +0100

    Build:Updating version to 0.0.6.

--- src/ndict_tools/__init__.py
+++ src/ndict_tools/__init__.py
@@ -1,5 +1,5 @@
 from .exception import NestedDictionaryException
 from .core import NestedDictionary
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __all__ = ["NestedDictionary", "NestedDictionaryException"]
