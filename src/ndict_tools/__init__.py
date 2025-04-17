6051a687b97616c305ed90d1aa9710b8b51881e8
biface <fabrice.moutte.wn@gmail.com>
2025-04-17 09:44:43 +0200

    Build:WIP:Updating module loading in __init__.py.

--- src/ndict_tools/__init__.py
+++ src/ndict_tools/__init__.py
@@ -1,7 +1,24 @@
 from .core import NestedDictionary
-from .exception import NestedDictionaryException
+from .exception import (
+    NestedDictionaryException,
+    StackedAttributeError,
+    StackedDictionaryError,
+    StackedIndexError,
+    StackedKeyError,
+    StackedTypeError,
+    StackedValueError,
+)
 
 __version__ = "0.7.0"
 __author__ = "biface"
 
-__all__ = ["NestedDictionary", "NestedDictionaryException"]
+__all__ = [
+    "NestedDictionary",
+    "NestedDictionaryException",
+    "StackedDictionaryError",
+    "StackedKeyError",
+    "StackedAttributeError",
+    "StackedTypeError",
+    "StackedValueError",
+    "StackedIndexError",
+]
