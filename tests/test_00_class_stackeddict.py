97695929a4706de638fbe00673b584044f9ff448
biface <fabrice.moutte.wn@gmail.com>
2025-07-27 19:16:50 +0200

    Test:WIP:Test has been improved and simplified using fixtures and parametrization.

--- tests/test_00_class_stackeddict.py
+++ tests/test_00_class_stackeddict.py
@@ -10,39 +10,67 @@ from ndict_tools.exception import (
 from ndict_tools.tools import _StackedDict, from_dict
 
 
+@pytest.fixture
+def stacked_dict():
+    return _StackedDict(default_setup={"indent": 0, "default_factory": None})
+
+
+# These tests must be suppressed in vers 1.2 of the package - managing deprecated parameters
+
+@pytest.mark.parametrize("parameters, expected, error, expected_error",
+                         [
+                             ({"indent": 10, "default": None}, [("indent", 10), ("default_factory", None)], False,
+                              None),
+                             ({"indent": 10}, [("indent", 10), ("default_factory", None)], False, None),
+                             ({"indent": 10, "default": _StackedDict},
+                              [("indent", 10), ("default_factory", _StackedDict)], False, None),
+                             ({"default": None}, [("indent", 10), ("default_factory", None)], True,
+                              StackedKeyError),
+
+                         ])
+def test_deprecated_parameters(parameters, expected, error, expected_error):
+    if not error:
+        dp = _StackedDict(**parameters)
+        for (attribute, value) in expected:
+            assert dp.__getattribute__(attribute) == value
+    else:
+        with pytest.raises(expected_error):
+            dp = _StackedDict(**parameters)
+
+
+# End of deprecated parameters tests
+
 def test_unused_error():
     e = StackedDictionaryError("This is an unused class", 1000)
     assert str(e) == "This is an unused class"
     assert e.error_code == 1000
 
-
-def test_stacked_dict_init_error():
+@pytest.mark.parametrize("parameters", [
+    {}, {"indent":0}, {"default_factory": None}
+])
+def test_stacked_dict_init_error(parameters):
     with pytest.raises(StackedKeyError):
-        _StackedDict()
-        _StackedDict(indent=0)
-        _StackedDict(default=None)
+        _StackedDict(default_setup=parameters)
 
 
-def test_stacked_dict_init_success():
-    sd = _StackedDict(indent=0, default=None)
-    assert isinstance(sd, _StackedDict)
-    assert sd.indent == 0
-    assert hasattr(sd, "default_factory")
-    assert sd.default_factory is None
+def test_stacked_dict_init_success(stacked_dict):
+    assert isinstance(stacked_dict, _StackedDict)
+    assert stacked_dict.indent == 0
+    assert hasattr(stacked_dict, "default_factory")
+    assert stacked_dict.default_factory is None
 
 
-def test_stacked_dict_any_keys():
-    sd = _StackedDict(indent=0, default=None)
-    sd[1] = "integer"
-    sd[(1, 2)] = "tuple"
-    assert sd[1] == "integer"
-    assert sd[(1, 2)] == "tuple"
+def test_stacked_dict_any_keys(stacked_dict):
+    stacked_dict[1] = "integer"
+    stacked_dict[(1, 2)] = "tuple"
+    assert stacked_dict[1] == "integer"
+    assert stacked_dict[(1, 2)] == "tuple"
 
 
-def test_stacked_dict_typeerror_key_dict():
-    sd = _StackedDict(indent=0, default=None)
+def test_stacked_dict_typeerror_key_dict(stacked_dict):
     with pytest.raises(TypeError):
-        assert sd[{1, 2}] == "dict"
+        assert stacked_dict[{1, 2}] == "dict"
+        assert stacked_dict[[1, [1, 2]]] == "dict"
 
 
 def test_from_dict():
@@ -56,13 +84,17 @@ def test_from_dict():
     assert nd.default_factory is None
 
 
-def test_unpacked_values():
-    sd = _StackedDict(indent=0, default=None)
-    sd[1] = "first"
-    sd[2] = {"first": 1, "second": 2}
-    sd[3] = 3
-    assert list(sd.unpacked_keys()) == [(1,), (2, "first"), (2, "second"), (3,)]
-    assert list(sd.unpacked_values()) == ["first", 1, 2, 3]
+def test_unpacked_values(stacked_dict):
+    stacked_dict[1] = "first"
+    stacked_dict[2] = {"first": 1, "second": 2}
+    stacked_dict[3] = 3
+    assert list(stacked_dict.unpacked_keys()) == [
+        (1,),
+        (2, "first"),
+        (2, "second"),
+        (3,),
+    ]
+    assert list(stacked_dict.unpacked_values()) == ["first", 1, 2, 3]
 
 
 def test_from_nested_dict():
@@ -89,43 +121,42 @@ def test_from_dict_attribute_error():
         from_dict(
             {1: "first", 2: {"first": 1, "second": 2}, 3: 3},
             NestedDictionary,
-            init={"indent": 2, "strict": True},
-            attributes={"factor": True},
+            default_setup={"indent": 0, "default_factory": None, "factor": True},
         )
+    with pytest.raises(StackedKeyError):
+        from_dict({1: "first", 2: {"first": 1, "second": 2}, 3: 3}, NestedDictionary)
 
 
-def test_shallow_copy_dict():
-    sd = _StackedDict(indent=0, default=None)
-    sd[1] = "Integer"
-    sd[(1, 2)] = "Tuple"
-    sd["2"] = {"first": 1, "second": 2}
-    sd_copy = sd.copy()
+def test_shallow_copy_dict(stacked_dict):
+    stacked_dict[1] = "Integer"
+    stacked_dict[(1, 2)] = "Tuple"
+    stacked_dict["2"] = {"first": 1, "second": 2}
+    sd_copy = stacked_dict.copy()
     assert sd_copy[1] == "Integer"
     assert sd_copy[(1, 2)] == "Tuple"
     assert isinstance(sd_copy["2"], dict)
     assert not isinstance(sd_copy["2"], _StackedDict)
     sd_copy[1] = "Changed in string"
-    assert sd[1] == "Integer"
+    assert stacked_dict[1] == "Integer"
     assert sd_copy[1] == "Changed in string"
     assert isinstance(sd_copy["2"]["second"], int)
-    sd["2"]["second"] = "3"
+    stacked_dict["2"]["second"] = "3"
     assert sd_copy["2"]["second"] == "3"
     assert isinstance(sd_copy["2"]["second"], str)
 
 
-def test_deep_copy_dict():
-    sd = _StackedDict(indent=0, default=None)
-    sd[1] = "Integer"
-    sd[(1, 2)] = "Tuple"
-    sd["2"] = {"first": 1, "second": 2}
-    sd_copy = sd.deepcopy()
+def test_deep_copy_dict(stacked_dict):
+    stacked_dict[1] = "Integer"
+    stacked_dict[(1, 2)] = "Tuple"
+    stacked_dict["2"] = {"first": 1, "second": 2}
+    sd_copy = stacked_dict.deepcopy()
     assert sd_copy[1] == "Integer"
     assert sd_copy[(1, 2)] == "Tuple"
     assert isinstance(sd_copy["2"], dict)
     sd_copy[1] = "Changed in string"
-    assert sd[1] == "Integer"
+    assert stacked_dict[1] == "Integer"
     assert sd_copy[1] == "Changed in string"
     assert isinstance(sd_copy["2"]["second"], int)
-    sd["2"]["second"] = "3"
+    stacked_dict["2"]["second"] = "3"
     assert sd_copy["2"]["second"] == 2
     assert isinstance(sd_copy["2"]["second"], int)
--- tests/test_01_class_strict.py
+++ tests/test_01_class_strict.py
@@ -11,40 +11,101 @@ Smooth option defines default_factory as the NestedDictionary that is to say :
 
 import pytest
 
-from ndict_tools import NestedDictionary
-
-nd = NestedDictionary(strict=True)
-nd["a"] = 1
-nd["b"] = NestedDictionary(strict=True)
-nd["b"]["a"] = 2
+from ndict_tools import NestedDictionary, StrictNestedDictionary
 
 
-def test_class_instance():
+@pytest.fixture(scope="module")
+def nd():
+    return NestedDictionary(
+        {"a": 1, "b": {"a": 2}}, default_setup={"indent": 0, "default_factory": None}
+    )
+
+
+@pytest.fixture(scope="module")
+def snd():
+    return StrictNestedDictionary(
+        {"a": 1, "b": {"a": 2}},
+        default_setup={"indent": 0, "default_factory": NestedDictionary},
+    )
+
+
+# Testing NestedDictionary with strict default factory (None)
+
+
+def test_class_instance(nd):
     assert isinstance(nd, NestedDictionary)
 
 
-def test_class_attributes_instances():
+def test_class_attributes_instances(nd):
     assert isinstance(nd["a"], int)
     assert isinstance(nd["b"]["a"], int)
+    assert isinstance(nd[["b", "a"]], int)
 
 
-def test_class_attributes_values():
+def test_class_attributes_values(nd):
     assert nd["a"] == 1
     assert nd["b"]["a"] == 2
+    assert nd[["b", "a"]] == 2
 
 
-def test_strict_option():
+def test_nested_strict_option(nd):
     assert hasattr(nd, "default_factory")
     assert nd.default_factory is None
 
 
-def test_strict_behavior():
+def test_nested_strict_behavior(nd):
     with pytest.raises(KeyError):
         value = nd["b"]["b"]
 
 
-def test_nested_nested():
-    nd_2 = NestedDictionary(nd, indent=2, strict=True)
+def test_nested(nd):
+    nd_2 = NestedDictionary(nd, default_setup={"indent": 2, "default_factory": None})
     assert nd.indent == 0
     assert nd_2.indent == 2
     assert nd_2["a"] == nd["a"]
+
+
+# Testing StrictNestedDictionary - default factory is always None
+
+
+def test_strict_class_none():
+    lnd = StrictNestedDictionary()
+    assert lnd.default_factory is None
+    assert lnd.indent == 0
+
+
+def test_strict_class_instance(snd):
+    assert isinstance(snd, StrictNestedDictionary)
+    assert isinstance(snd, NestedDictionary)
+
+
+def test_strict_class_attributes_instances(snd):
+    assert isinstance(snd["a"], int)
+    assert isinstance(snd["b"]["a"], int)
+    assert isinstance(snd[["b", "a"]], int)
+
+
+def test_strict_class_attributes_values(snd):
+    assert snd["a"] == 1
+    assert snd["b"]["a"] == 2
+    assert snd[["b", "a"]] == 2
+
+
+def test_strict_class_option(snd):
+    assert hasattr(snd, "default_factory")
+    assert snd.default_factory is None
+
+
+def test_strict_class_behavior(snd):
+    with pytest.raises(KeyError):
+        value = snd["b"]["b"]
+
+
+def test_strict_nested(snd):
+    nd_2 = StrictNestedDictionary(
+        snd, default_setup={"indent": 2, "default_factory": None}
+    )
+    assert snd.indent == 0
+    assert nd_2.indent == 2
+    assert nd_2["a"] == snd["a"]
+    assert nd_2.default_factory is None
