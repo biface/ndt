75ad401417b1ae5fd38fdf78dcb7d0b892ea6aa9
biface <fabrice.moutte.wn@gmail.com>
2025-07-27 15:52:58 +0200

    Build:WIP:Updating module __init__.py

--- src/ndict_tools/__init__.py
+++ src/ndict_tools/__init__.py
@@ -1,4 +1,4 @@
-from .core import NestedDictionary
+from .core import NestedDictionary, SmoothNestedDictionary, StrictNestedDictionary
 from .exception import (
     NestedDictionaryException,
     StackedAttributeError,
@@ -14,6 +14,8 @@ __author__ = "biface"
 
 __all__ = [
     "NestedDictionary",
+    "SmoothNestedDictionary",
+    "StrictNestedDictionary",
     "NestedDictionaryException",
     "StackedDictionaryError",
     "StackedKeyError",
